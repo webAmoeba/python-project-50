20b9e99fe269bbba316fc03264bff1ce88257a56
Valera Galeev <71322091+webAmoeba@users.noreply.github.com>
2024-12-30 12:41:08 +0400

    feat: add support for the --format option to select output format in CLI

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,11 +6,16 @@ def main():
         prog="gendiff",
         description="Compares two configuration files and shows a difference.",
     )
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
+    parser.add_argument("first_file", help="Path to the first file")
+    parser.add_argument("second_file", help="Path to the second file")
+    parser.add_argument(
+        "-f", "--format", dest="format", default="plain",
+        help="set format of output"
+    )
     args = parser.parse_args()
-    print(f"First file: {args.first_file}")
-    print(f"Second file: {args.second_file}")
+
+    print(f"Comparing '{args.first_file}' and '{args.second_file}'")
+    print(f"Output format: {args.format}")
 
 
 if __name__ == "__main__":
