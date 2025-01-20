e67a5e1556763ddb3ef54d558d40930258d463c6
Valera Galeev <71322091+webAmoeba@users.noreply.github.com>
2025-01-20 14:04:42 +0400

    feat: implement file reading functionality
    and update main script to use it

---
+++ gendiff/read_file.py
@@ -0,0 +1,5 @@
+import json
+
+
+def read_file(file):
+    return json.load(open(file))
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,7 @@
 import argparse
 
+from gendiff.read_file import read_file
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -15,6 +17,11 @@ def main():
         help="set format of output",
     )
     args = parser.parse_args()
+    
+    file1 = read_file(args.first_file)
+    file2 = read_file(args.second_file)
+
+    print(file1, file2, sep="\n")
 
 
 if __name__ == "__main__":
